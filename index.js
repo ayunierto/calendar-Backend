473d313b7a3d7ce4a48f28e5b6e985cbd1359a88
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-07-14 06:40:28 -0500

    configuring express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('asd')
+const express = require("express")
+
+
+// Create express server
+const app = express()
+
+
+// Routes
+app.get("/", ( req, res ) => {
+
+    res.json({
+        ok: true,
+    })
+
+})
+
+
+// listen requests
+app.listen( 4000, () => {
+    console.log(`Server on port: ${ 4000 }`)
+})
