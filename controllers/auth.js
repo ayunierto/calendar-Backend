c86182a07f204bba3d0cc490cb2757b972666a5a
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-07-17 18:17:54 -0500

    users validate

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,11 +7,22 @@ const createUser = async ( req, res ) => {
 
     try {
 
-        const user = new User( req.body )
+        let user = await User.findOne({ email })
+
+        if ( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The mail is already in use',
+            })
+        }
+
+        user = new User( req.body )
         await user.save()
+
         res.status(201).json({
             ok: true,
-            msg: 'register',
+            uid: user.id,
+            name: user.name
         })
 
     } catch (error) {
