a2ef5892693ea7edc429abd22331bf72cfc3f6b8
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-07-17 18:06:21 -0500

    create user in DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,26 @@
 const { validationResult } = require("express-validator")
+const User = require("../models/User")
 
-const createUser = ( req, res ) => {
+const createUser = async ( req, res ) => {
 
     const { name, email, password } = req.body
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
+    try {
+
+        const user = new User( req.body )
+        await user.save()
+        res.status(201).json({
+            ok: true,
+            msg: 'register',
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: true,
+            msg: 'Error ocurred',
+        })
+    }
 }
 
 const login = ( req, res ) => {
